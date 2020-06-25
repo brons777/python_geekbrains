3948c6f494a912038ac9ee7bb4ec7534808db5ef
Nikita_Lyapin <lyapin2010@yandex.ru>
2020-06-25 23:18:24 +0300

    task_1

---
+++ task_1.py
@@ -0,0 +1,12 @@
+int_1 = 10
+float_1 = 2.6
+string_1 = 'Hi'
+bool_1 = False
+
+print(int_1, ' ', float_1, ' ', string_1, ' ', bool_1)
+
+int_1 = int(input('Введите целое число: '))
+float_1 = float(input('Введите вещественное число: '))
+string_1 = input('Введите строку: ')
+
+print(int_1, ' ', float_1, ' ', string_1)
