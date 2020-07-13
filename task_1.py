24544b1ed2bca59fa46ddde43bc25a924b36906a
Nikita_Lyapin <lyapin2010@yandex.ru>
2020-07-13 23:22:24 +0300

    task_1

---
+++ task_1.py
@@ -0,0 +1,24 @@
+import itertools
+import time
+
+
+class TrafficLight:
+    def __init__(self, color):
+        self.__color = color
+
+    def running(self):
+        for i in itertools.cycle(self.__color):
+            print(i)
+            if i == 'red':
+                time.sleep(7)
+            elif i == 'yellow':
+                time.sleep(2)
+            else:
+                time.sleep(8)
+
+
+color = ['red', 'yellow', 'green']
+
+my_trLight = TrafficLight(color)
+my_trLight.running()
+# print(my_trLight._TrafficLight__color)
